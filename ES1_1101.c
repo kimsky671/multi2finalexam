771a8316f36177b71d6064cc14d48d8a550a6106
김하늘 <haneul9513@naver.com>
2025-11-26 17:37:25 +0900

    ES1 주석수정

--- ES1_1101.c
+++ ES1_1101.c
@@ -11,15 +11,12 @@
 int ES1action(int hp, char* name) {
     printf(">>> [%s (mm)] 턴 (현재 HP: %d) <<<\n", name, hp);
 
-    // ES1의 전략: HP가 30 이하일 때 회복을 시도하고, 그 외에는 공격(Wait)을 시도합니다.
     if (hp <= 30) {
         printf("[%s]가 위험을 느껴 '긴급 회복'을 시도합니다!\n", name);
-        // 조건: return Heal(hp, name);
         return Heal(hp, name);
     }
     else {
         printf("[%s]가 상대에게 '강력한 공격'을 시도합니다.\n", name);
-        // 조건: return Wait(hp, name);
         return Wait(hp, name);
     }
 } 
