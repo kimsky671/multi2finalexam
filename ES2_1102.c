95a966bf28460a03f6a5f2de49d91d19bfb214fe
rladbwls77 <K51165480@gmail.com>
2025-11-26 17:42:44 +0900

    수정본

--- ES2_1102.c
+++ ES2_1102.c
@@ -1,16 +1,17 @@
-﻿#include "ES2_1102.h" 
+﻿//20251390,김유진
+#include "ES2_1102.h" 
 
- // --- ES2action 함수 정의 ---
+
 int ES2action(int hp, char* name)
 {
 
     if (hp < 30) {
-        // Heal 함수는 Action.obj에 정의되어 있습니다.
+      
         return Heal(hp, name);
     }
    
     else {
-        // Wait 함수는 Action.obj에 정의되어 있습니다.
+      
         return Heal(hp, name);
     }
 }
